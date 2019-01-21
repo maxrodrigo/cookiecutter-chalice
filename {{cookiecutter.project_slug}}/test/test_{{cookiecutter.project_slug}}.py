7d5bdb20a79060e88d07fd907d95820caa13aa39
Max Rodrigo <contact@maxrodrigo.com>
2019-01-21 16:59:43 +0100

    Fix cc test case

--- {{cookiecutter.project_slug}}/test/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/test/test_{{cookiecutter.project_slug}}.py
@@ -6,6 +6,6 @@ import json
 
 
 def test_response(client):
-    resp = client.handle_request(method="GET", path="/", headers={}, body="")
+    resp = client.get("/")
     assert resp["statusCode"] == 200
     assert json.loads(resp["body"]) == {"hello": "world"}
